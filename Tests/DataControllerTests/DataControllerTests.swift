6c97b280563d8e5c5e129b31d61727f408f270b2
gavinmorrow <gavinfmorrow@gmail.com>
2022-07-25 19:28:33 -0400

    Fix tests

--- Tests/DataControllerTests/DataControllerTests.swift
+++ Tests/DataControllerTests/DataControllerTests.swift
@@ -6,6 +6,5 @@ final class DataControllerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DataController().text, "Hello, World!")
     }
 }
